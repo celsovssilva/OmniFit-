753806fd4b3cd655a7fa2e2e88bd03cd860f8ad0
celsovssilva <celsoviniciussouzasilva@gmail.com>
2026-09-02 13:35:51 -0300

    criando token e verificação da assinatura finalizado

--- backend/src/main/java/com/example/backend/service/impl/TokenServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/TokenServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.backend.service.impl;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.example.backend.entity.User;
 import com.example.backend.service.TokenService;
-import com.nimbusds.jose.Algorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Date;
 
 @Service
 public class TokenServiceImpl implements TokenService {
@@ -14,13 +20,33 @@ public class TokenServiceImpl implements TokenService {
     private Long expiration;
     @Override
     public String generateToken(User user) {
-        Algorithm
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        Instant dataExpiracao = Instant.now().plusMillis(expiration);
+            try{
+                var tokenCreate = JWT.create()
+                        .withIssuer("api-fit")
+                    .withClaim("role",user.getTipoPerfil().name())
+                    .withSubject(user.getEmail())
+                    .withClaim("id", user.getId())
+                    .withExpiresAt(dataExpiracao);
 
-        return "";
+                    return tokenCreate.sign(algorithm);
+            } catch (JWTCreationException e) {
+            throw new RuntimeException("erro ao criar token" + e);
+}
     }
 
     @Override
     public String getSubject(String jwt) {
-        return "";
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("api-fit")
+                    .build()
+                    .verify(jwt)
+                    .getSubject();
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("erro ao verificar token" + e);
+        }
     }
 }
